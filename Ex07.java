cbbe3c08aa7fc1dd7fe10ad3f30d446d83ca0c6c
Victor Anhaya <victoranhaya8@icloud.com>
2024-03-23 22:41:36 -0300

    Exercicio 7

---
+++ Ex07.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+    public class Ex07 {
+        public static void executar() {
+            Scanner leitor = new Scanner(System.in);
+
+            System.out.println("Digite um número: ");
+            int numero = leitor.nextInt();
+
+            if (numero >= 100 && numero <= 200) {
+                System.out.println("O número está dentro do intervalo entre 100 e 200.");
+            } else {
+                System.out.println("O número está fora do intervalo entre 100 e 200.");
+            }
+            
+        }
+
+}
