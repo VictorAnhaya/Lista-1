6aa90c31634ffad35ac863726fc74ecff587b6e3
Victor Anhaya <victoranhaya8@icloud.com>
2024-03-23 22:36:10 -0300

    Exercicio 6

---
+++ Ex06.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class Ex06 {
+        public static void executar() {
+            Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em graus Celsius: ");
+        double celsius = leitor.nextDouble();
+
+        double fahrenheit = (9 * celsius + 160) / 5;
+
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
+
+       ;
+    }
+
+}
