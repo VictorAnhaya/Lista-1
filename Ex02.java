673e2979bd5f12a394c00baa9544aa3480a00e01
Victor Anhaya <victoranhaya8@icloud.com>
2024-03-23 21:45:56 -0300

    Exercicio 02

--- Ex02.java
+++ Ex02.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class Ex02 {
     public static void executar() {
-        Scanner leitor = new Scanner(System.in);
+      Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro numero: ");
-        
+        System.out.print("Digite o primeiro número: ");
+        int numero1 = leitor.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int numero2 = leitor.nextInt();
+
+        int soma = numero1 + numero2;
+
+        System.out.println("A soma dos números é: " + soma);
 
     }
-
 }
